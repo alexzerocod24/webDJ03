8f314bd5a38ac4ddc19b90821199aeaa994a1d27
alexzerocod24 <ustvich.alex@gmail.com>
2025-04-07 20:34:26 +0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,11 +1,7 @@
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
 
-    path('', views.index, name='home'),
-    path('new', views.new, name='page2'),
-    path('user', views.user, name='page4'),
-
+    path('', views.home, name='news_home'),
 ]
--- views.py
+++ views.py
@@ -1,16 +1,6 @@
 from django.shortcuts import render
+from .models import News_post
 
-
-def index(request):
-    data = {
-        'caption':"airplanes"
-    }
-    return render(request,  'main/index.html', data)
-
-def new(request):
-    return render(request,  'main/new.html')
-
-def user(request):
-    return render(request,  'main/user.html')
-
-
+def home(request):
+    news = News_post.objects.all()
+    return render(request, 'news/news.html', { 'news': news})
